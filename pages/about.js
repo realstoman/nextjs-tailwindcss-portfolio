8428ddba73e14ad0f5441e413ee394d2fed21e85
NangialaiStoman <nstoman7@gmail.com>
2021-12-18 22:19:30 +0430

    Add contact and about page

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+const about = () => {
+	return (
+		<div>
+			<h1>About Page</h1>
+		</div>
+	);
+};
+
+export default about;
---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+const contact = () => {
+	return (
+		<div>
+			<h1>Contact Page</h1>
+		</div>
+	);
+};
+
+export default contact;
