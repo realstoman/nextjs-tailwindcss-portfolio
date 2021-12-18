82125ed8d7e98b7f4ec25591fb7e678f56e444a6
NangialaiStoman <nstoman7@gmail.com>
2021-12-18 19:07:37 +0430

    Add default layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import DefaultLayout from '../components/layout/DefaultLayout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<DefaultLayout>
+			<Component {...pageProps} />
+		</DefaultLayout>
+	);
 }
 
-export default MyApp
+export default MyApp;
