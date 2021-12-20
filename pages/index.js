12c029ede31139578e99e1f89104377c15da9e3a
realstoman <realstoman@gmail.com>
2021-12-20 18:23:40 +0430

    Create theme switcher hook

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import AppBanner from '../components/shared/AppBanner';
 
 export default function Home() {
 	return (
-		<div className={styles.container}>
-			<h1>Home page</h1>
+		<div className="container mx-auto">
+			<AppBanner />
 		</div>
 	);
 }
