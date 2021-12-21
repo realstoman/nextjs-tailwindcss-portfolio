11e754c69203926dc97eeea0f029c5266d1da4fc
NangialaiStoman <nstoman7@gmail.com>
2021-12-21 15:56:34 +0430

    Create meta component

--- components/layout/DefaultLayout.js
+++ components/layout/DefaultLayout.js
@@ -1,9 +1,11 @@
 import AppHeader from '../shared/AppHeader';
 import AppFooter from '../shared/AppFooter';
+import PagesMetaHead from '../PagesMetaHead';
 
 const DefaultLayout = ({ children }) => {
 	return (
 		<>
+			<PagesMetaHead />
 			<AppHeader />
 			<div>{children}</div>
 			<AppFooter />
