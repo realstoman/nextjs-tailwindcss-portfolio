6491af2edd3ecf510020326fa1d9341885b776dd
NangialaiStoman <nstoman7@gmail.com>
2021-12-22 15:06:17 +0430

    Fix navigation responsive issue

--- components/shared/AppBanner.js
+++ components/shared/AppBanner.js
@@ -11,7 +11,7 @@ function AppBanner() {
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
-			className="flex flex-col sm:justify-between items-center sm:flex-row mt-12 md:mt-2"
+			className="flex flex-col sm:justify-between items-center sm:flex-row mt-5 md:mt-2"
 		>
 			<div className="w-full md:w-1/3 text-left">
 				<motion.h1
