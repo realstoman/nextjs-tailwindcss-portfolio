d467bafce62eecfae72d73ed33f60070037de6de
realstoman <realstoman@gmail.com>
2022-01-27 18:40:02 +0430

    Create button reusable component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import PagesMetaHead from '../components/PagesMetaHead';
 import ProjectsGrid from '../components/projects/ProjectsGrid';
 import AppBanner from '../components/shared/AppBanner';
+import Button from '../components/reusable/Button';
 
 export default function Home() {
 	return (
@@ -12,9 +13,9 @@ export default function Home() {
 			<ProjectsGrid />
 
 			<div className="mt-10 sm:mt-15 flex justify-center">
-				<div className="flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl font-medium">
-					<Link href="/projects" aria-label="More Projects">
-						More Projects
+				<div className="font-general-medium flex items-center px-6 py-3 rounded-lg shadow-lg hover:shadow-xl bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 text-white text-lg sm:text-xl duration-300">
+					<Link href="/projects" aria-label="More Projects" passHref>
+						<Button title="More Projects" />
 					</Link>
 				</div>
 			</div>
