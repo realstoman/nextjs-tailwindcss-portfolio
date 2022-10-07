7f99236e86e9cb28ede95b8d66915305615404b2
WorldWideGreg <greg_hehe@yahoo.fr>
2022-10-07 21:04:34 +0200

    Mise à jours CV + bug footer

--- components/shared/AppFooterCopyright.js
+++ components/shared/AppFooterCopyright.js
@@ -4,7 +4,7 @@ function AppFooterCopyright() {
       <div className="text-lg text-ternary-dark dark:text-ternary-light">
         &copy; {new Date().getFullYear()}
         <a
-          href="https://github.com/WorldWideGreg/GregoryMorelPortfolioV2"
+          href="https://myportfoliov2-beta.vercel.app/"
           target="__blank"
           className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
         >
